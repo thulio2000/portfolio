b62f5a4aefba85459a9cf74f6c761a4e2fd6ae61
Lucas Túlio <lucastuliolima@gmail.com>
2022-10-30 17:38:26 -0300

    "Adding BrowserRouter"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.scss';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/Home';
 import About from './components/About';
@@ -8,13 +8,13 @@ import Contact from './components/Contact';
 function App() {
   return (
     <>
-      <Routes>
+      <BrowserRouter>
         <Route path="/" element={< Layout />} >
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
         </Route>
-      </Routes>
+      </BrowserRouter>
     </>
   );
 }
