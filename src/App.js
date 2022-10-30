e7537ce035842f1079f9f02fa51a2002b9766224
Lucas Túlio <lucastuliolima@gmail.com>
2022-10-30 17:47:52 -0300

    "Switching BrowserRouter for HashRouter"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.scss';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/Home';
 import About from './components/About';
@@ -8,13 +8,13 @@ import Contact from './components/Contact';
 function App() {
   return (
     <>
-      <BrowserRouter basename="/portfolio">
+      <Routes>
         <Route path="/" element={< Layout />} >
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
         </Route>
-      </BrowserRouter>
+      </Routes>
     </>
   );
 }
